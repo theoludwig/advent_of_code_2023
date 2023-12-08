bf03a758c34f09b2c695bc4cfdcbb4bec5daf315
Théo LUDWIG <contact@theoludwig.fr>
2023-12-08 18:29:39 +0100

    feat: add day 4

---
+++ day_4/src/main.rs
@@ -0,0 +1,8 @@
+use day_4::{part_1, part_2};
+
+fn main() {
+    let input = include_str!("../input.txt");
+    println!("- Day 4: Scratchcards -");
+    println!("Answer Part 1: {}", part_1(input));
+    println!("Answer Part 2: {}", part_2(input));
+}
