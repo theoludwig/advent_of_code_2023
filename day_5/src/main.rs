45eca9020df063cb850e0b1df4795364fbf5fda2
Théo LUDWIG <contact@theoludwig.fr>
2023-12-09 20:22:46 +0100

    feat: add day 5

---
+++ day_5/src/main.rs
@@ -0,0 +1,8 @@
+use day_5::{part_1, part_2};
+
+fn main() {
+    let input = include_str!("../input.txt");
+    println!("- Day 5: If You Give A Seed A Fertilizer -");
+    println!("Answer Part 1: {}", part_1(input));
+    println!("Answer Part 2: {}", part_2(input));
+}
